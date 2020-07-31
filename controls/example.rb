dad00b947fad6008fd32935a955e723e02a95485
Jim Quilty <jquilty@chef.io>
2020-07-31 09:28:35 -0400

    test effortless
    Signed-off-by: Jim Quilty <jquilty@chef.io>

--- controls/example.rb
+++ controls/example.rb
@@ -1,18 +1,10 @@
 # copyright: 2018, The Authors
 
-title "sample section"
+title 'Apache Controls'
 
-# you can also use plain tests
-describe file("/tmp") do
-  it { should be_directory }
-end
+include_controls 'apache-baseline'
 
-# you add controls here
-control "tmp-1.0" do                        # A unique ID for this control
-  impact 0.7                                # The criticality, if this control fails.
-  title "Create /tmp directory"             # A human-readable title
-  desc "An optional description..."
-  describe file("/tmp") do                  # The actual test
-    it { should be_directory }
-  end
-end
+# include_controls 'cis-centos7-level1-server' do
+#   require_control 'xccdf_org.cisecurity.benchmarks_rule_1.1.16_Ensure_nosuid_option_set_on_devshm_partition'
+#   #skip_control 'xccdf_org.cisecurity.benchmarks_rule_1.1.14_Ensure_nodev_option_set_on_home_partition'
+# end
---
+++ habitat/plan.sh
@@ -0,0 +1,8 @@
+pkg_name=apache-baseline
+pkg_version=1.0.0
+pkg_origin=chef
+pkg_scaffolding="chef/scaffolding-chef-inspec"
+scaffold_automate_server_url='https://192.168.42.106'             # Example: https://foo.bar.com
+scaffold_automate_user='admin'                                    # Example: "admin"
+scaffold_automate_token='03XQwyOiNVfw2L_kDfisrwCtYaw='            # Example: "DI0WVxInnyGnWKRlZBGizTXySgk="
+scaffold_compliance_insecure=true                                 # set true to ignore SSL cert error when retrieving profile from A2
